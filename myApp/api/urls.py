9d3f518f8d3e22fe6a7b668b415cd6d291821986
Chrisfufu <zuofu@ualberta.ca>
2020-04-30 16:55:19 -0600

    add controllers to the projects.

--- myApp/api/urls.py
+++ myApp/api/urls.py
@@ -5,5 +5,8 @@ from .views import *
 from django.conf import settings
 
 urlpatterns = [
-    
+    url(r'^create/$', FeedDuckInfoCreateAPIView.as_view(), name='create'),
+    url(r'^all/$', FeedDuckInfoListAPIView.as_view(), name='view'),
+    url(r'^update/(?P<pk>[0-9]+)/$', FeedDuckInfoUpdateAPIView.as_view(), name='update'),
+    url(r'^info/(?P<pk>[0-9]+)/delete/$', FeedDuckInfoDeteleAPIView.as_view(), name='delete'),
 ]
