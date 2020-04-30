bf4de59b317ba12b7e6ef75a0f0006eb91f37f02
Chrisfufu <zuofu@ualberta.ca>
2020-04-30 16:40:48 -0600

    update model

--- myApp/models.py
+++ myApp/models.py
@@ -4,16 +4,16 @@ from django.utils.crypto import get_random_string
 from django.core.validators import RegexValidator
 from django.core.validators import validate_email
 # Create your models here.
-
+# import datetime
 
 class FeedDuckInfo(models.Model):
     id = models.AutoField(primary_key=True)
-    time = DateTimeField(auto_now=True)
-    food = models.TextField(null=True)
-    location = models.TextField(null=True)
-    numberOfDucks = models.IntegerField(null=True)
-    foodType = models.TextField(null=True)
-    foodCalories = models.FloatField(null=True)
+    time = models.DateTimeField()
+    food = models.TextField()
+    location = models.TextField()
+    numberOfDucks = models.IntegerField()
+    foodType = models.TextField()
+    foodCalories = models.FloatField()
 
     class Meta:
         verbose_name = "FeedDuckInfo"
