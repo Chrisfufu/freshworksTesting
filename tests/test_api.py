4ff36dcc0c3b2ce3420dff5b89cea3f8111c8dbd
Chrisfufu <zuofu@ualberta.ca>
2020-05-01 04:44:06 -0600

    edit tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,7 @@ import time
 
 def test_case1():
     # create your test case 1
-    r = requests.get('http://localhost:8000/api/info/all/')
+    print('\n\n\nthere\n\n\n')
+    r = requests.get('http://127.0.0.1:8000/api/info/all/')
     print('\n\n\n',r,'here\n\n\n')
     assert(r.status_code == 200)
