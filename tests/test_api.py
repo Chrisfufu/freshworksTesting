8aa321d5cb45fd683654d27b6227d8bf6cae9ed3
Chrisfufu <zuofu@ualberta.ca>
2020-05-01 11:09:18 -0600

    see if github action could fetch from google

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,6 @@ import time
 def test_case1():
     # create your test case 1
     print('\n\n\nthere\n\n\n')
-    r = requests.get('http://127.0.0.1:8000/api/info/all/')
+    r = requests.get('https://www.google.ca/')
     print('\n\n\n',r,'here\n\n\n')
     assert(r.status_code == 200)
