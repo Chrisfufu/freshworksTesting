04b334cbf83d2c6a982eb7d41999b4a398eb323d
Chrisfufu <zuofu@ualberta.ca>
2020-05-01 12:25:57 -0600

    now should work

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,5 @@ import time
 
 def test_case1():
     # create your test case 1
-    print('\n\n\nthere\n\n\n')
     r = requests.get('http://0.0.0.0:8000/api/info/all/')
-    print('\n\n\n',r,'here\n\n\n')
-    assert(r.status_code == 500)
+    assert(r.status_code == 200)
