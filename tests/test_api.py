ba35dca309c9a0332d00ce1014de0373cf5ab934
Chrisfufu <zuofu@ualberta.ca>
2020-05-01 03:49:10 -0600

    add pytest example

---
+++ tests/test_api.py
@@ -0,0 +1,10 @@
+import requests
+import pytest # have to install via command "pip3 install -U pytest"
+import json
+
+import time
+
+def test_case1():
+    # create your test case 1
+    r = requests.get('http://localhost:8000/api/info/all/')
+    assert(r.status_code == 200)
