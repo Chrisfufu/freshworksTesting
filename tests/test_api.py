92aee4a8fb3e56f5407e4ade6688f5acec6765dd
Chrisfufu <zuofu@ualberta.ca>
2020-05-01 14:09:22 -0600

    update tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,3 +9,9 @@ def test_case1():
     r = requests.get('http://0.0.0.0:8000')
     print(r)
     assert(r.status_code == 404)
+
+
+def test_case2():
+    r = requests.get('http://0.0.0.0:8000/api/info/all')
+    assert(r.status_code == 500)
+    print(r)
