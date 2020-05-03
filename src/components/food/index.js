b679bc41c6e895f01ff53af7f7a281cbfd33eac1
Chrisfufu <zuofu@ualberta.ca>
2020-05-02 22:42:55 -0600

    add comments

--- src/components/food/index.js
+++ src/components/food/index.js
@@ -13,29 +13,45 @@ const layout = {
     span: 8,
   },
 };
-
+// connect to store.
 @connect((store) => {
   return {
     error: store.food.error,
     food: store.food.foods,
   };
 })
+
+// this is the second tab
+// it is a form by using Ant Design.
+// this tab creates a food object once at a time by clicking the submit button.
+// shows success popup window when successfully upload the food.
 class FoodForm extends React.Component {
+  // these are state in the class
   state = { visible: false };
+
+  // use: it is used for open the popup window.
+  // in ant design, Modal is the term for popup window.
+  // visible state is the state to control turn on or turn off the popup window
   showModal = () => {
     this.setState({
       visible: true,
     });
   };
 
+  // customize the ok button when the Modal opens
+  // after turn off the Modal, it calls onReset method.
+  // it is a callback method for setState.
+  // onReset will clear the form
   handleOk = e => {
     this.setState({
         visible: false,
       },()=>{ this.onReset() }
     );
-
   };
 
+  // handleSubmit calls the API in the action.
+  // pass three parameters to the addFood method,
+  // it only pass all the values to the method
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -45,6 +61,8 @@ class FoodForm extends React.Component {
     });
   };
 
+  // react lifecycle, when finished uploading the information of feeding the ducks,
+  // then showModal
   UNSAFE_componentWillReceiveProps(nextProps) {
     if(this.props.food !== nextProps.food &&
       nextProps.food !== undefined)
@@ -54,6 +72,7 @@ class FoodForm extends React.Component {
 
   }
 
+  // this method is used for reset the form.
   onReset = () => {
     this.props.form.resetFields();
   };
@@ -62,73 +81,93 @@ class FoodForm extends React.Component {
     const { getFieldDecorator } = this.props.form;
     return (
       <AppLayout style={{overflow: "auto"}}>
-          <Form {...layout} onSubmit={this.handleSubmit} >
-            <Form.Item className="formSection">
-              {getFieldDecorator('food', {
-                rules: [{ required: true, message: 'Please input the food name!' }],
-              })(
-                <Input
-                  prefix={<Icon type="apple" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Food Name"
-                />,
-              )}
-            </Form.Item>
-            <Form.Item className="formSection">
-              {getFieldDecorator('foodType', {
-                rules: [{ required: true, message: 'Please input food type!' }],
-              })(
-                <Input
-                  prefix={<Icon type="tags" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  type="foodType"
-                  placeholder="Food Type"
-                />,
-              )}
-            </Form.Item>
-            <Form.Item className="formSection">
-              {getFieldDecorator('foodCalories', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input food calories!'
-                  },
-                  {
-                    type: 'number',
-                    Message: 'Please enter a number/float',
-                    transform:(value)=> {return Number(value)}
-                  },
-                ],
-              })(
-                <Input
-                  prefix={<Icon type="calculator" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  type="foodCalories"
-                  placeholder="Food Calories"
-                />,
-              )}
-            </Form.Item>
-            <Form.Item
-              className="buttonCenter"
-              wrapperCol={{ ...layout.wrapperCol, offset: 4 }}
-            >
-              <Button type="primary" htmlType="submit" >
-                Submit
-              </Button>
+        {/* create a form */}
+        <Form {...layout} onSubmit={this.handleSubmit} >
 
-              <Button type="primary" htmlType="button" onClick={this.onReset}>
-                Reset
-              </Button>
-            </Form.Item>
-          </Form>
-          <Modal
-            title="Basic Modal"
-            visible={this.state.visible}
-            footer={[
-              <Button key="back" onClick={this.handleOk}>
-                OK
-              </Button>
-            ]}
+          {/* form item, this is the food name */}
+          <Form.Item className="formSection">
+            {getFieldDecorator('food', {
+              rules: [{ required: true, message: 'Please input the food name!' }],
+            })(
+              <Input
+                prefix={<Icon type="apple" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Food Name"
+              />,
+            )}
+          </Form.Item>
+
+          {/*
+            get the food type from users
+          */}
+          <Form.Item className="formSection">
+            {getFieldDecorator('foodType', {
+              rules: [{ required: true, message: 'Please input food type!' }],
+            })(
+              <Input
+                prefix={<Icon type="tags" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="foodType"
+                placeholder="Food Type"
+              />,
+            )}
+          </Form.Item>
+
+          {/*
+            get How much food the ducks are fed
+            it has to be a number/float
+          */}
+          <Form.Item className="formSection">
+            {getFieldDecorator('foodCalories', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input food calories!'
+                },
+                {
+                  type: 'number',
+                  Message: 'Please enter a number/float',
+                  transform:(value)=> {return Number(value)}
+                },
+              ],
+            })(
+              <Input
+                prefix={<Icon type="calculator" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="foodCalories"
+                placeholder="Food Calories"
+              />,
+            )}
+          </Form.Item>
+
+          {/*
+            these are buttons to submit the answers or reset the form
+          */}
+          <Form.Item
+            className="buttonCenter"
+            wrapperCol={{ ...layout.wrapperCol, offset: 4 }}
           >
-            <p>You have successfully created a food</p>
-          </Modal>
+            <Button type="primary" htmlType="submit" >
+              Submit
+            </Button>
+
+            <Button type="primary" htmlType="button" onClick={this.onReset}>
+              Reset
+            </Button>
+          </Form.Item>
+        </Form>
+
+        {/*
+          this is the popup window: Modal
+        */}
+        <Modal
+          title="Success"
+          visible={this.state.visible}
+          footer={[
+            <Button key="back" onClick={this.handleOk}>
+              OK
+            </Button>
+          ]}
+        >
+          <p>You have successfully created a food</p>
+        </Modal>
       </AppLayout>
     );
   }
--- src/components/info/index.js
+++ src/components/info/index.js
@@ -16,25 +16,40 @@ const layout = {
 };
 
 const { Option } = Select;
+
+// connect to store.
 @connect((store) => {
   return {
     foodsFetched: store.food.foodsFetched,
     posted: store.info.posted
   };
 })
+// this is the first tab
+// it is a form by using Ant Design.
+// this tab creates a feeding information object once at a time
+// by clicking the submit button.
+// shows success popup window when successfully upload the information.
 class InfoForm extends React.Component {
+  // these are state in the class
   state = {
     visible: false,
     startTime: null,
-    endTime: null,
-    foods:[]
+    endTime: null
   };
+
+  // use: it is used for open the popup window.
+  // in ant design, Modal is the term for popup window.
+  // visible state is the state to control turn on or turn off the popup window
   showModal = () => {
     this.setState({
       visible: true,
     });
   };
 
+  // customize the ok button when the Modal opens
+  // after turn off the Modal, it calls onReset method.
+  // it is a callback method for setState.
+  // onReset will clear the form
   handleOk = e => {
     this.setState({
         visible: false,
@@ -42,6 +57,12 @@ class InfoForm extends React.Component {
     );
 
   };
+
+  // onChange is used for catching the changes for the DatePicker
+  // and then pass the date String to start time and end time.
+  // the requirements said that we need to have a duriation for
+  // the date, so, this DatePicker allows users to select a range of date.
+  // and pick time.
   onChange = (date, dateString)=> {
     this.setState({
         startTime: dateString[0],
@@ -49,6 +70,11 @@ class InfoForm extends React.Component {
       });
   }
 
+  // handleSubmit calls the API in the action.
+  // pass three parameters to the addInfo method,
+  // 1. values in the form,
+  // 2. startTime in the dateString
+  // 3. endTime in the dateString
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -58,40 +84,55 @@ class InfoForm extends React.Component {
     });
   };
 
+  // react lifecycle, this is to fetch all the foods in the database,
+  // then it can display all food in the selection options.
   componentDidMount(){
     this.props.dispatch(fetchFood())
   }
 
-  UNSAFE_componentWillUpdate(nextProps) {
-    if(this.props.foodsFetched !== nextProps.foodsFetched &&
-      nextProps.foodsFetched !== undefined)
-    {
-      this.setState({
-        foods: nextProps.foodsFetched,
-      });
-    }
-
+  // react lifecycle, when finished uploading the information of feeding the ducks,
+  // then showModal
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if(this.props.posted !== nextProps.posted &&
       nextProps.posted !== undefined)
       {
         this.showModal()
       }
-
   }
 
+  // this method is used for reset the form.
   onReset = () => {
     this.props.form.resetFields();
   };
 
   render() {
     const { getFieldDecorator } = this.props.form;
-    const { foods } = this.state;
+    const { foodsFetched } = this.props;
     return (
+
       <AppLayout style={{overflow: "auto"}}>
+      {/* AppLayout, includes header, navigation bar, content, footer */}
+          {/*
+            create a form
+          */}
           <Form {...layout} onSubmit={this.handleSubmit} >
-            <Form.Item className="formSection">
+
+            {/*
+              DatePicker, this is used to pick a date and time,
+
+              meet the requirement:
+              Nice to have: the ability for a little old lady who feeds the ducks
+              every day in the same way to set a repeating schedule
+              so she doesn’t have to use the application every day
+            */}
+            <Form.Item help="Pick a range to start feeding ducks"
+              className="formSection">
               {getFieldDecorator('time', {
-                rules: [{ type: 'array', required: true }],
+                rules: [{
+                  type: 'array',
+                  required: true,
+                  message: 'Please input the Time Period!'
+                }],
               })(
                 <DatePicker.RangePicker
                   onChange={this.onChange}
@@ -102,6 +143,10 @@ class InfoForm extends React.Component {
                   />
               )}
             </Form.Item>
+
+            {/*
+              where the ducks are fed
+            */}
             <Form.Item className="formSection">
               {getFieldDecorator('location', {
                 rules: [{ required: true, message: 'Please input the location!' }],
@@ -112,6 +157,10 @@ class InfoForm extends React.Component {
                 />,
               )}
             </Form.Item>
+
+            {/*
+              collect how many ducks are fed
+            */}
             <Form.Item className="formSection">
               {getFieldDecorator('numberOfDucks', {
                 rules: [{ required: true, message: 'Please input number of ducks!' }],
@@ -120,6 +169,14 @@ class InfoForm extends React.Component {
               )}
             </Form.Item>
 
+            {/*
+              select foods that are used to feed the ducks,
+              the food already contains how much are fed (calories, or we could understand it as kg)
+
+              foods are being fetched from backend (database), then
+              shows all the foods to here.
+              users could search by food names.
+            */}
             <Form.Item className="formSection">
               {getFieldDecorator('foodSelected', {
                 rules: [{ required: true, message: 'Please select foods' }],
@@ -128,15 +185,15 @@ class InfoForm extends React.Component {
                   mode="multiple"
                   style={{ width: '100%' }}
                   placeholder="Select foods"
-                  optionLabelProp="label"
+                  optionFilterProp="label"
                 >
-                {foods.map(d => (
+                {foodsFetched.map(d => (
                   <Option
                     key={d.foodId}
                     value={d.foodId}
-                    label={d.food +" Calories:"+ d.foodCalories}
+                    label={d.food +", Calories:"+ d.foodCalories}
                   >
-                    {d.food +" Calories:"+ d.foodCalories}
+                    {d.food +", Calories:"+ d.foodCalories}
                   </Option>
                 ))}
                 </Select>
