8325b5908265a8f5084f04ff6d84a528a0b5ff13
Chrisfufu <li491645426@gmail.com>
2020-06-30 16:15:03 -0600

    add pytests

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,169 +7,58 @@ import time
 def test_get_all_info():
     # create your test case 1
     r = requests.get('http://localhost:8000/api/info/all/')
+    print(r)
     assert(r.status_code == 200)
 
-def test_get_all_foods():
-    r = requests.get('http://localhost:8000/api/foods/all/')
-    assert(r.status_code == 200)
-
-def test_post_first_foods():
-    url = 'http://localhost:8000/api/foods/create/'
+def test_post_key():
+    url = 'http://localhost:8000/api/info/create/'
     myobj = {
-        "food": "Apple",
-        "foodType": "Food",
-        "foodCalories": 100
+        "name": "Apple",
+        "description": "Food",
+        "expiryTime": "2006-10-25 14:30:59"
     }
     r = requests.post(url, data = myobj)
     assert(r.status_code == 201)
 
-def test_post_second_foods():
-    url = 'http://localhost:8000/api/foods/create/'
+def test_post_key_case_1():
+    url = 'http://localhost:8000/api/info/create/'
     myobj = {
-        "food": "Banana",
-        "foodType": "Food",
-        "foodCalories": 200.1
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 201)
-
-def test_post_foods_foodCalories():
-    url = 'http://localhost:8000/api/foods/create/'
-    myobj = {
-        "food": "Banana",
-        "foodType": "Food",
-        "foodCalories": "abc"
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 400)
-
-def test_post_foods_miss_foodType_foodCalories():
-    url = 'http://localhost:8000/api/foods/create/'
-    myobj = {
-        "food": "Banana",
+        "name": "Apple",
     }
     r = requests.post(url, data = myobj)
     assert(r.status_code == 500)
 
-def test_post_foods_miss_foodCalories():
-    url = 'http://localhost:8000/api/foods/create/'
+def test_post_key_case_2():
+    url = 'http://localhost:8000/api/info/create/'
     myobj = {
-        "food": "Banana",
-        "foodType": "Food",
+        "description": "Food",
     }
     r = requests.post(url, data = myobj)
     assert(r.status_code == 500)
 
-def test_post_foods_foodType():
-    url = 'http://localhost:8000/api/foods/create/'
+def test_post_key_case_3():
+    url = 'http://localhost:8000/api/info/create/'
     myobj = {
-        "food": "Banana",
-        "foodType": 1,
+        "expiryTime": "2020-06-20"
+    }
+    r = requests.post(url, data = myobj)
+    assert(r.status_code == 400)
+
+def test_post_key_case_4():
+    url = 'http://localhost:8000/api/info/create/'
+    myobj = {
+        "name": "Apple",
+        "description": "Food",
     }
     r = requests.post(url, data = myobj)
     assert(r.status_code == 500)
 
-def test_post_foods_foodCalories2():
-    url = 'http://localhost:8000/api/foods/create/'
-    myobj = {
-        "food": "Banana",
-        "foodType": 1,
-        "foodCalories": "abc"
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 400)
 
-def test_get_one_foods():
-    url = 'http://localhost:8000/api/foods/1/'
-    r = requests.get(url)
-    assert(r.status_code == 200)
-
-def test_post_info():
+def test_post_key_case_5():
     url = 'http://localhost:8000/api/info/create/'
     myobj = {
-        "food": [
-            {
-                "foodId": 1
-            },
-            {
-                "foodId": 2
-            }
-        ],
-        "time": "2020-05-10T00:12:00-06:00",
-        "location": "Edmonton",
-        "numberOfDucks": 2,
-        "repeatDays": 1
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 201)
-
-def test_post_info_without_foods():
-    url = 'http://localhost:8000/api/info/create/'
-    myobj = {
-        "time": "2020-05-10T00:12:00-06:00",
-        "location": "Edmonton",
-        "numberOfDucks": 2,
-        "repeatDays": 1
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 201)
-
-def test_post_info_numberOfDucks():
-    url = 'http://localhost:8000/api/info/create/'
-    myobj = {
-        "time": "2020-05-10T00:12:00-06:00",
-        "location": "Edmonton",
-        "numberOfDucks": "2",
-        "repeatDays": 1
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 201)
-
-def test_post_info_repeatDays():
-    url = 'http://localhost:8000/api/info/create/'
-    myobj = {
-        "time": "2020-05-10T00:12:00-06:00",
-        "location": "Edmonton",
-        "numberOfDucks": 3,
-        "repeatDays": "abc"
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 400)
-
-def test_post_info_location():
-    url = 'http://localhost:8000/api/info/create/'
-    myobj = {
-        "time": "2020-05-10T00:12:00-06:00",
-        "location": 2,
-        "numberOfDucks": 3,
-        "repeatDays": 1
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 201)
-
-def test_post_info_only_time():
-    url = 'http://localhost:8000/api/info/create/'
-    myobj = {
-        "time": "2020-05-10T00:12:00-06:00",
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 400)
-
-def test_post_info_only_time_location():
-    url = 'http://localhost:8000/api/info/create/'
-    myobj = {
-        "time": "2020-05-10T00:12:00-06:00",
-        "location": 2,
-    }
-    r = requests.post(url, data = myobj)
-    assert(r.status_code == 400)
-
-def test_post_info_without_repeatDays():
-    url = 'http://localhost:8000/api/info/create/'
-    myobj = {
-        "time": "2020-05-10T00:12:00-06:00",
-        "location": 2,
-        "numberOfDucks": 3,
+        "description": "Food",
+        "expiryTime": "2020-06-20"
     }
     r = requests.post(url, data = myobj)
     assert(r.status_code == 400)
